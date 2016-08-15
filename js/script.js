e8185d70446a32e86b4da5fc4d5f6a63a6cee102
Grzegorz Pach <Grzegorz Pach>
2016-08-15 23:15:51 +0200

    reset styling, added basic mouse events

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-$(document).ready(function () {
-    var context = document.getElementById('canvas').getContext('2d');
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var canvas = document.getElementById('canvas');
+canvas.setAttribute('width', $(window).width());
+canvas.setAttribute('height', $(window).height()-5);
+var context = canvas.getContext('2d');
+var paint;
+
+
+$('#canvas').mousedown(function (e) {
+    var mouseX = e.pageX - this.offsetLeft;
+    var mouseY = e.pageY - this.offsetTop;
+    paint = true;
+    addClick(mouseX, mouseY);
+    redraw();
+});
+$('#canvas').mousemove(function (e) {
+    if (paint) {
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+        redraw();
+    }
+});
+$('canvas').mouseup(function () {
+    paint = false;
+});
+$('canvas').mouseleave(function () {
+    paint = false;
 });
